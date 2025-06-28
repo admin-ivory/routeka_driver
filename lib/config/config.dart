89cc64a126f9356aa80a66a906f711b4f710d3cc
admin-ivory <ivoiryproject@gmail.com>
2025-06-28 12:52:03 +0200

    init Driver

---
+++ lib/config/config.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: camel_case_types
+
+class config{
+
+
+  String baseUrl2 = "https://routeka.afroo-app.com";
+
+  String baseUrl3 = "https://kpanel.routeka.com";
+
+  String baseUrl = "https://routeka.afroo-app.com/Driver_Api";
+
+
+}
---
+++ lib/config/language_string.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+
+class KdriverTranslate extends Translations{
+  @override
+  // TODO: implement keys
+  Map<String, Map<String, String>> get keys => {
+    'en_English' :{
+      'Login':"Login"
+    },
+    'fr_French' :{
+      'Login': 'Connexion'
+    }
+  };
+
+
+}
